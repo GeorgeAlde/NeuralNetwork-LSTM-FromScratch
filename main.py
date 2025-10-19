92c278bcdc3207578720fd8badfce7674b5c6679
GeorgeAlde <galderman19@gmail.com>
2025-10-19 22:14:15 +0100

    implemented backpropagation and updated main to train the neural network

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import numpy as np
+from random import choice
 from net import Network
 from data import get_data, get_answers
 
@@ -7,14 +8,57 @@ def softmax(x1, x2): #returns softmax of x1
     return np.exp(x1) / (np.exp(x1) + np.exp(x2))
 
 def loss(output, correct_index):
-    return -np.log(output[correct_index])
+    if output[correct_index] != 0:
+        return -np.log(output[correct_index])
+    else:
+        return 10
 
 
-mydata = get_data()
-survival = get_answers()
+
 net = Network()
-for i in range(len(mydata)):
-    out = net.forward(mydata[i])
-    softout = softmax(out[0], out[1]),softmax(out[1], out[0])
-    L = loss(softout, survival[i][1])
-    print(L)
+
+def train():
+    num_correct = 0
+    last100 = 0
+    count = 0
+    mydata = get_data()
+    survival = get_answers()
+    surv_count_correct = 0
+    surv_count = 0
+    for i in range(len(mydata)):
+        out = net.forward(mydata[i])
+        correct_index = survival[i][1]
+        softout = softmax(out[0], out[1]),softmax(out[1], out[0])
+
+        L = loss(softout, correct_index)
+
+        d_y = [0,0]
+        for j in range(len(softout)):
+            if survival[i][1] == j:
+                d_y[j] = softout[j] - 1
+            else:
+                d_y[j] = softout[j]
+        for k in range(2):
+            net.backprop(d_y[k], k, learn_rate=0.0005, gradient_num=100)
+        if np.argmax(softout) == correct_index:
+            num_correct += 1
+        if np.argmax(softout) == correct_index:
+            if correct_index == 1:
+                surv_count_correct += 1
+            last100 += 1
+        if np.argmax(softout) == 1:
+            surv_count += 1
+        count += 1
+
+
+        if count == 100:
+            print(f'{last100} out of last 100 correct')
+            count = 0
+            last100 = 0
+    print(surv_count_correct, surv_count)
+    count = 0
+    for i in range(len(survival)):
+        if (survival[i][1]) == 1:
+            count += 1
+    print(count, len(survival))
+train()
--- net.py
+++ net.py
@@ -7,30 +7,65 @@ class Network:
     def __init__(self):
         self.Wxh = randn(7, 7) / 1000
         self.Why = randn(2, 7) / 1000
-        #print(self.Why)
-        self.bh = np.array([0]*7)
-        self.by1 = 0
-        self.by2 = 0
+        #print(self.Wxh[1])
+        self.bh = [0,0,0,0,0,0,0]
+        self.by = [0,0]
+        self.last_hs = []
+        self.last_inputs = []
 
     def forward(self, inputs): #input is a 7 long list
         xh = [0] * 7
-        for Hi in range(7):
-            for j in range(len(inputs)):
-                xh[Hi] += self.Wxh[Hi][j] * inputs[j]
-            xh[Hi] = xh[Hi] + self.bh[Hi]
-            xh[Hi] = np.tanh(xh[Hi])
+        self.last_hs = []
+        self.last_inputs = []
+        for i in range(7):
+            for Hi in range(7):
+                xh[i] += self.Wxh[i][Hi] * inputs[Hi]
+            xh[i] = xh[i] + self.bh[i]
+            xh[i] = np.tanh(xh[i])
+            self.last_hs.append(xh[i])
+            self.last_inputs = inputs
             #print(self.Wxh[Hi][j])
+        y0 = 0
         y1 = 0
-        y2 = 0
         for Hi in range(7):
-            y1 += self.Why[0][Hi] * xh[Hi]
-            y2 += self.Why[1][Hi] * xh[Hi]
-        y1 += self.by1
-        y2 += self.by2
-        return y1, y2
+            y0 += self.Why[0][Hi] * xh[Hi]
+            y1 += self.Why[1][Hi] * xh[Hi]
+        y0 += self.by[0]
+        y1 += self.by[1]
+        return y0, y1
 
-    def backprop(self, d_y, learn_rate = 2e-2, ):
-        epochs = 1000
+    def backprop(self, d_y, y_index, gradient_num = 1000, learn_rate = 2e-2):
+        d_by = d_y
+        d_by = np.clip(d_by, -1, 1)
+        for i in range(gradient_num):
+            for j in range(7):
+                d_why = d_y * self.last_hs[j]
+                d_why = np.clip(d_why, -1, 1)
+                d_bh = d_y * self.Why[y_index,j]
+                d_bh = np.clip(d_bh, -1, 1)
+                for k in range(7):
+                    d_wxh = d_y * self.Why[y_index, j] * self.last_inputs[k]
+                    d_wxh = np.clip(d_wxh, -1 , 1)
+
+                    step = d_wxh * learn_rate
+                    self.Wxh[k,j] -= step
+
+
+                step = d_why * learn_rate
+                self.Why[y_index, j] -= step
+                #print(self.Why)
+                step = d_bh * learn_rate
+                self.bh[j] -= step
+
+        step = d_by * learn_rate
+        self.by[y_index] -= step
+
+
+
+
+
+
+
         
 
 
