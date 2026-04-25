Fixed key bugs in backprop

main.py
@@ -4,15 +4,13 @@ from data import get_data, get_answers
 from net import Network
 
 
-def softmax(x1, x2):  # returns softmax of x1
-    return np.exp(x1) / (np.exp(x1) + np.exp(x2))
+def softmax(x1, x2):
+    ex = np.exp([x1, x2])
+    return ex / np.sum(ex)
 
 
 def loss(output, correct_index):
-    if output[correct_index] != 0:
-        return -np.log(output[correct_index])
-    else:
-        return 10 
+    return -np.log(output[correct_index] + 1e-9)
 
 
 net = Network()
@@ -22,34 +20,37 @@ def train():
     num_correct = 0
     last100 = 0
     count = 0
+
     mydata = get_data()
     survival = get_answers()
-    for i in range(len(mydata)):
-        out = net.forward(mydata[i])
-        correct_index = survival[i][1]
-        softout = softmax(out[0], out[1]), softmax(out[1], out[0])
 
-        L = loss(softout, correct_index)
+    for rep in range(10):
 
-        d_y = [0, 0]
-        for j in range(len(softout)):
-            if survival[i][1] == j:
-                d_y[j] = softout[j] - 1
-            else:
-                d_y[j] = softout[j]
-        for k in range(2):
-            net.backprop(d_y[k], k, learn_rate=0.001)
-        if np.argmax(softout) == correct_index:
-            num_correct += 1
-        if np.argmax(softout) == correct_index:
-            last100 += 1
-        count += 1
+        for i in range(len(mydata)):
 
-        if count == 100:
-            print(f'{last100} out of last 100 correct')
-            count = 0
-            last100 = 0
+            out = net.forward(mydata[i])
 
+            correct_index = int(survival[i][1])
+
+            softout = softmax(out[0], out[1])
+
+            d_y = softout.copy()
+            d_y[correct_index] -= 1
+
+            L = loss(softout, correct_index)
+
+            net.backprop(d_y)
+
+            if np.argmax(softout) == correct_index:
+                num_correct += 1
+                last100 += 1
+
+            count += 1
+
+            if count == 100:
+                print(f'{last100} out of last 100 correct')
+                count = 0
+                last100 = 0
 
 
 train()

net.py
@@ -5,67 +5,73 @@ from numpy.random import randn
 class Network:
 
     def __init__(self):
-        self.Wxh = randn(7, 7) / 1000
-        self.Why = randn(2, 7) / 1000
+        self.Wxh = randn(7, 7) / np.sqrt(7)
+        self.Why = randn(2, 7) / np.sqrt(7)
 
-        self.bh = np.array([0,0,0,0,0,0,0])
-        self.by = np.array([0,0])
-        self.last_hs = np.array([])
-        self.last_inputs = np.array([])
+        self.bh = np.zeros(7)
+        self.by = np.zeros(2)
 
-    def forward(self, inputs): #input is a 7 long list
-        xh = [0] * 7
+        self.last_hs = np.zeros(7)
+        self.last_inputs = np.zeros(7)
+
+        self.last_out = np.zeros(2)
+
+    def forward(self, inputs):
+
+        self.last_inputs = np.array(inputs, dtype=float)
+
+        xh = np.zeros(7, dtype=float)
+
         for i in range(7):
-            for Hi in range(7):
-                xh[i] += self.Wxh[i][Hi] * inputs[Hi]
-            xh[i] = xh[i] + self.bh[i]
-            xh[i] = np.tanh(xh[i])
-            self.last_hs = np.append(self.last_hs, xh[i])
-            self.last_inputs = inputs
-
-        y0 = 0
-        y1 = 0
-        for Hi in range(7):
-            y0 += self.Why[0][Hi] * xh[Hi]
-            y1 += self.Why[1][Hi] * xh[Hi]
-        y0 += self.by[0]
-        y1 += self.by[1]
-        return y0, y1
-
-    def backprop(self, d_y, y_index, gradient_num = 20, learn_rate = 0.0005):
-        d_by = d_y
-        d_by = np.clip(d_by, -1, 1)
-        for i in range(gradient_num):
             for j in range(7):
-                d_why = d_y * self.last_hs[j]
-                d_why = np.clip(d_why, -1, 1)
-                d_bh = d_y * self.Why[y_index,j]
-                d_bh = np.clip(d_bh, -1, 1)
-                for k in range(7):
-                    d_wxh = d_y * self.Why[y_index, j] * self.last_inputs[k]
-                    d_wxh = np.clip(d_wxh, -1 , 1)
+                xh[i] += self.Wxh[i][j] * inputs[j]
 
-                    step = d_wxh * learn_rate
-                    self.Wxh[k,j] -= step
+            xh[i] += self.bh[i]
+            xh[i] = np.tanh(xh[i])
 
+        self.last_hs = xh.copy()
 
-                step = d_why * learn_rate
-                self.Why[y_index, j] -= step
-                step = d_bh * learn_rate
-                self.bh[j] -= step
+        y = np.zeros(2)
 
-        step = d_by * learn_rate
-        self.by[y_index] -= step
+        for i in range(2):
+            for j in range(7):
+                y[i] += self.Why[i][j] * xh[j]
+
+            y[i] += self.by[i]
+
+        self.last_out = y.copy()
+        return y
 
 
+    def backprop(self, d_y, learn_rate=0.0005):
 
+        self.by -= learn_rate * d_y
 
+        d_why = np.zeros_like(self.Why)
 
+        for i in range(2):
+            for j in range(7):
+                d_why[i][j] = d_y[i] * self.last_hs[j]
 
+        self.Why -= learn_rate * d_why
 
-        
+        dh = np.zeros(7)
 
+        for j in range(7):
+            for i in range(2):
+                dh[j] += self.Why[i][j] * d_y[i]
 
+            dh[j] *= (1 - self.last_hs[j] ** 2)
 
+        d_wxh = np.zeros_like(self.Wxh)
+        d_bh = np.zeros_like(self.bh)
 
+        for j in range(7):
+            d_bh[j] = dh[j]
+
+            for k in range(7):
+                d_wxh[j][k] = dh[j] * self.last_inputs[k]
+
+        self.Wxh -= learn_rate * d_wxh
+        self.bh -= learn_rate * d_bh
 